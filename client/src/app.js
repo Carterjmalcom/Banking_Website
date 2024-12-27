bfad226cb77acf3d0aaf31ab0b1fbbe22dbe5db0
Carterjmalcom <144744476+Carterjmalcom@users.noreply.github.com>
2024-12-27 22:57:49 +0000

    navbar is smooth across all pages

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,31 @@
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
+let isActive =  sessionStorage.getItem("active")
+main.classList.remove("main-oc-animation")
+setTimeout(() => {
+    main.classList.add("main-oc-animation")
+}, 250);
+
+console.log(isActive)
+if(isActive=="false"){
+navigation.classList.toggle("active");
+main.classList.toggle("active");
+console.log("test")
+}
 
 toggle.onclick = function () {
     navigation.classList.toggle("active");
     main.classList.toggle("active");  
+    if (navigation.classList.contains("active")){
+        sessionStorage.setItem("active", false)
+        isActive =  sessionStorage.getItem("active")
+        console.log(isActive) 
+    } else {
+        sessionStorage.setItem("active", true)
+        isActive =  sessionStorage.getItem("active")
+        console.log(isActive) 
+    }
 };
 
 
