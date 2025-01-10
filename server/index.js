a9a1c680276c255c629318c104ac3bb72b7c65b0
twaters27 <144744407+twaters27@users.noreply.github.com>
2025-01-10 21:29:07 +0000

    changed lot

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Define paths
 const clientPath = path.join(__dirname, '..', 'client/src');
-const dataPath = path.join(__dirname, 'data', 'customers.json');
+const dataPath = path.join(__dirname, 'data', 'users.json');
 const serverPublic = path.join(__dirname, 'public');
 const serverPages = path.join(__dirname, 'public/pages');
 
@@ -20,15 +20,15 @@ app.use(express.json());
 // Routes
 
 
-// customers route
-app.get('/customers', async (req, res) => {
+// users route
+app.get('/user', async (req, res) => {
     try {
         const data = await fs.readFile(dataPath, 'utf-8');
-        const customers = JSON.parse(data);
-        if (!customers) {
+        const users = JSON.parse(data);
+        if (!users) {
             throw new Error("Error: no users available");
         }
-        res.status(200).json(customers);
+        res.status(200).json(users);
     } catch (error) {
         console.error("Problem getting the users: " + error.message);
         res.status(500).json({ error: "Problem reading users" });
@@ -87,25 +87,25 @@ app.get('/sign-up', (req, res) => {
 app.post('/submit-form', async (req, res) => {
     try {
         const { email, password } = req.body;
-        let customers = [];
+        let users = [];
         try {
             const data = await fs.readFile(dataPath, 'utf8');
-            customers = JSON.parse(data);
+            users = JSON.parse(data);
         } catch (error) {
             console.error("Error reading user data:", error)
-            customers = [];
+            users = [];
         }
 
-        let customer = customers.find(u => u.email === email);
-        if (customer) {
+        let user = users.find(u => u.email === email);
+        if (user) {
             console.log("already an account")
         } else {
-            customer = { email, password };
-            customers.push(customer);
+            user = { email, password };
+            users.push(user);
             console.log("already an account")
         }
 
-        await fs.writeFile(dataPath, JSON.stringify(customers, null, 2));
+        await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
         res.status(200).send("Successfully created an account")
 
     } catch (error) {
@@ -127,15 +127,15 @@ app.put('/update-user/:currentEmail/:currentPassword', async (req, res) => {
         console.log('New user data:', { newEmail, newPassword });
         const data = await fs.readFile(dataPath, 'utf8');
         if (data) {
-            let customers = JSON.parse(data);
-            const customerIndex = customers.findIndex(customer => customer.email === currentEmail && user.password === currentPassword);
-            console.log(customerIndex);
-            if (customerIndex === -1) {
+            let users = JSON.parse(data);
+            const userIndex = users.findIndex(user => user.email === currentEmail && user.password === currentPassword);
+            console.log(userIndex);
+            if (userIndex === -1) {
                 return res.status(404).json({ message: "User not found" })
             }
-            customers[customerIndex] = { ...customers[customerIndex], email: newEmail, password: newPassword };
-            console.log(customers);
-            await fs.writeFile(dataPath, JSON.stringify(customers, null, 2));
+            users[userIndex] = { ...users[userIndex], email: newEmail, password: newPassword };
+            console.log(users);
+            await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
 
             res.status(200).json({ message: `You sent ${newEmail} and ${newPassword}` });
         }
@@ -151,29 +151,29 @@ app.delete('/user/:email/:password', async (req, res) => {
     try {
         const { email, password } = req.params
         // initalize an empty array of 'users'
-        let customers = [];
+        let users = [];
         // try to read the users.json file and cache as data
         try {
             const data = await fs.readFile(dataPath, 'utf-8');
-            customers = JSON.parse(data);
+            users = JSON.parse(data);
         } catch (error) {
-            return res.status(404).send('Customers data not found')
+            return res.status(404).send('users data not found')
         }
         // cache the userIndex based on a matching name and email
-        const customerIndex = customers.findIndex(customer => customer.email === email && customer.password === password);
-        console.log(customerIndex);
-        if (customerIndex === -1) {
+        const userIndex = users.findIndex(user => user.email === email && user.password === password);
+        console.log(userIndex);
+        if (userIndex === -1) {
             return res.status(404).send('User not found');
         }
         // splice the users array with the intended delete name and email
-        customers.splice(userIndex, 1);
+        users.splice(userIndex, 1);
         try {
-            await fs.writeFile(dataPath, JSON.stringify(customers, null, 2));
+            await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
         } catch (error) {
             console.error("Failed to write to database");
         }
         // send a success deleted message
-        res.send('Customer deleted successfully');
+        res.send('user deleted successfully');
     } catch (error) {
         res.status(500).send('There was an error deleting user');
     }
